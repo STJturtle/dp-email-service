793f8f79fb40627af203ce943464ba243dd7d309
arnav-turtlemint <arnav.satrusal@turtlemint.com>
2023-11-27 18:33:02 +0530

    wellness payment links

--- src/main/java/com/tester/localtester/model/XtestModel.java
+++ src/main/java/com/tester/localtester/model/XtestModel.java
@@ -1,18 +1,59 @@
 package com.tester.localtester.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
 import java.util.Map;
 
-public class XtestModel {
+@Getter
+@Setter
+@NoArgsConstructor
+public class XtestModel implements Serializable {
+
+    public String businessType;
+    public String productCode;
+    public String planType;
+    public String planCode;
+    public Integer policyTerm;
+    public String insurerCode;
+    public String InsuredTitle;
+    public String insuredfirstName;
+    public String insuredlastName;
+    public String insuredEmail;
+    public String insuredgender;
+    public String insuredmobile;
+    public String insureddob;
+    public String city;
     public String pincode;
+    public String address1;
+    public String Phone;
     public Map<String, Map<String, Boolean>> plans;
 
     public String getPincode() {
         return pincode;
     }
 
+//    public String getProductCode() {
+//        return productCode;
+//    }
+//    public String getBusinessType() {
+//        return businessType;
+//    }
+
     public void setPincode(String pincode) {
         this.pincode = pincode;
     }
+//    public void setProductCode(String productCode) {
+//        this.productCode = productCode;
+//    }
+//    public void setBusinessType(String businessType) {
+//        this.businessType = businessType;
+//    }
 
     public Map<String, Map<String, Boolean>> getPlans() {
         return plans;
--- src/main/java/com/tester/localtester/services/Xtest.java
+++ src/main/java/com/tester/localtester/services/Xtest.java
@@ -1,6 +1,7 @@
 package com.tester.localtester.services;
 
 import com.tester.localtester.model.XtestModel;
+import lombok.Data;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -16,20 +17,27 @@ public class Xtest {
 
         Map<String, String> mailData = new HashMap<>();
 
-        String fileDirectory = "/Users/abhishekjaiswal/Desktop/Turtlemint/dp-email-service/";
-        Reader csvFile = new FileReader(fileDirectory + "wellness-2.csv");
+        String fileDirectory = "/Users/arnavsatrusal/Downloads/";
+        Reader csvFile = new FileReader(fileDirectory + "Wellness_test_data_2.csv");
 
         String[] HEADERS = {
-                "Pincode",
-                "Digital_Plan",
-                "Acti_Health",
-                "Acti_Health_Plus",
-                "Health_Assure_One",
-                "Health_Assure_One_Plus",
-                "OPD_Hero",
-                "OPD_Plus",
-                "OPD_Premium",
-                "OPD_Elite"
+            "businessType",
+            "productCode",
+            "pincode",
+            "planCode",
+            "policyTerm",
+            "insurerCode",
+            "InsuredTitle",
+            "insuredfirstName",
+            "insuredlastName",
+            "insuredEmail",
+            "insuredgender",
+            "insuredmobile",
+            "insureddob",
+            "city",
+            "pincode",
+            "address1",
+            "Phone"
         };
 
         final String digitalPlanConstant = "digital_Plan";
@@ -63,41 +71,45 @@ public class Xtest {
 
         for (CSVRecord record : records) {
             XtestModel model = new XtestModel();
-            String pincode = record.get("Pincode").trim();
-            String digiPlan = record.get("Digital_Plan").trim();
-            String actiHealthPlan = record.get("Acti_Health").trim();
-            String actiHealthPlusPlan = record.get("Acti_Health_Plus").trim();
-            String healthAssureOnePlan = record.get("Health_Assure_One").trim();
-            String healthAssureOnePlusPlan = record.get("Health_Assure_One_Plus").trim();
+            String businessType = record.get("businessType").trim();
+            String productCode = record.get("productCode").trim();
+            String pincode = record.get("pincode").trim();
+//            String digiPlan = record.get("Digital_Plan").trim();
+//            String actiHealthPlan = record.get("Acti_Health").trim();
+//            String actiHealthPlusPlan = record.get("Acti_Health_Plus").trim();
+//            String healthAssureOnePlan = record.get("Health_Assure_One").trim();
+//            String healthAssureOnePlusPlan = record.get("Health_Assure_One_Plus").trim();
+//
+//            String opdHero = record.get(svaasOPDHeroConstant).trim();
+//            String opdPlus = record.get(svaasOPDPlusConstant).trim();
+//            String opdPremium = record.get(svaasOPDPremiumConstant).trim();
+//            String opdElite = record.get(svaasOPDEliteConstant).trim();
 
-            String opdHero = record.get(svaasOPDHeroConstant).trim();
-            String opdPlus = record.get(svaasOPDPlusConstant).trim();
-            String opdPremium = record.get(svaasOPDPremiumConstant).trim();
-            String opdElite = record.get(svaasOPDEliteConstant).trim();
+//            if (pincode.equals("Pincode")) continue;
 
-            if (pincode.equals("Pincode")) continue;
+//            if (digiPlan.length() > 0) ekinCareItems.put(digitalPlanConstant, true);
+//            if (actiHealthPlan.length() > 0) ekinCareItems.put(actiHealthConstant, true);
+//            if (actiHealthPlusPlan.length() > 0) ekinCareItems.put(actiHealthPlusConstant, true);
+//            if (healthAssureOnePlan.length() > 0) healthAssureItems.put(healthAssureOneConstant, true);
+//            if (healthAssureOnePlusPlan.length() > 0) healthAssureItems.put(healthAssureOnePlusConstant, true);
+//
+//            if (opdHero.length() > 0) svaasItems.put(svaasOPDHeroConstant, true);
+//            if (opdPlus.length() > 0) svaasItems.put(svaasOPDPlusConstant, true);
+//            if (opdPremium.length() > 0) svaasItems.put(svaasOPDPremiumConstant, true);
+//            if (opdElite.length() > 0) svaasItems.put(svaasOPDEliteConstant, true);
 
-            if (digiPlan.length() > 0) ekinCareItems.put(digitalPlanConstant, true);
-            if (actiHealthPlan.length() > 0) ekinCareItems.put(actiHealthConstant, true);
-            if (actiHealthPlusPlan.length() > 0) ekinCareItems.put(actiHealthPlusConstant, true);
-            if (healthAssureOnePlan.length() > 0) healthAssureItems.put(healthAssureOneConstant, true);
-            if (healthAssureOnePlusPlan.length() > 0) healthAssureItems.put(healthAssureOnePlusConstant, true);
-
-            if (opdHero.length() > 0) svaasItems.put(svaasOPDHeroConstant, true);
-            if (opdPlus.length() > 0) svaasItems.put(svaasOPDPlusConstant, true);
-            if (opdPremium.length() > 0) svaasItems.put(svaasOPDPremiumConstant, true);
-            if (opdElite.length() > 0) svaasItems.put(svaasOPDEliteConstant, true);
-
-            plans.put(eKinCare, ekinCareItems);
-            plans.put(healthAssure, healthAssureItems);
-            plans.put(svaas, svaasItems);
-
+//            plans.put(eKinCare, ekinCareItems);
+//            plans.put(healthAssure, healthAssureItems);
+//            plans.put(svaas, svaasItems);
+//
             model.setPincode(pincode);
-            model.setPlans(plans);
-            plans = new HashMap<>();
-            ekinCareItems = new HashMap<>();
-            healthAssureItems = new HashMap<>();
-            svaasItems = new HashMap<>();
+            model.setProductCode(productCode);
+            model.setBusinessType(businessType);
+//            model.setPlans(plans);
+//            plans = new HashMap<>();
+//            ekinCareItems = new HashMap<>();
+//            healthAssureItems = new HashMap<>();
+//            svaasItems = new HashMap<>();
 
             lists.add(model);
         }
@@ -110,30 +122,29 @@ public class Xtest {
         File tempFile = new File(fileDirectory + "tempfile.json");
         PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
         String line = null;
-        pw.println("use turtlefin;");
-        pw.println("db.wellnessTestXYZ.insertMany([");
+        pw.println("[");
 
         for (var val : lists) {
-            line = "{\"pincode\":\"" + val.getPincode() + "\", \"plans\":" + val.getPlans() + "},";
-            line = line.replace("=", ":");
-            line = line.replace("ekinCare", "\"ekinCare\"");
-            line = line.replace("svaas", "\"svaas\"");
-            line = line.replace("healthAssure", "\"healthAssure\"");
-
-            line = line.replace("acti_Health_Plus", "\"acti_Health_Plus\"");
-            line = line.replace("acti_Health:", "\"acti_Health\":");
-            line = line.replace("digital_Plan", "\"digital_Plan\"");
-
-            line = line.replace("health_Assure_One:", "\"health_Assure_One\":");
-            line = line.replace("health_Assure_One_Plus", "\"health_Assure_One_Plus\"");
-
-            line = line.replace(svaasOPDEliteConstant, "\"opd_Elite\"");
-            line = line.replace(svaasOPDPremiumConstant, "\"opd_Premium\"");
-            line = line.replace(svaasOPDPlusConstant, "\"opd_Plus\"");
-            line = line.replace(svaasOPDHeroConstant, "\"opd_Hero\"");
+            line = "{\"businessType\":\"" + val.pincode + "\", \"productCode\":" + val.productCode + "},";
+//            line = line.replace("=", ":");
+//            line = line.replace("ekinCare", "\"ekinCare\"");
+//            line = line.replace("svaas", "\"svaas\"");
+//            line = line.replace("healthAssure", "\"healthAssure\"");
+//
+//            line = line.replace("acti_Health_Plus", "\"acti_Health_Plus\"");
+//            line = line.replace("acti_Health:", "\"acti_Health\":");
+//            line = line.replace("digital_Plan", "\"digital_Plan\"");
+//
+//            line = line.replace("health_Assure_One:", "\"health_Assure_One\":");
+//            line = line.replace("health_Assure_One_Plus", "\"health_Assure_One_Plus\"");
+//
+//            line = line.replace(svaasOPDEliteConstant, "\"opd_Elite\"");
+//            line = line.replace(svaasOPDPremiumConstant, "\"opd_Premium\"");
+//            line = line.replace(svaasOPDPlusConstant, "\"opd_Plus\"");
+//            line = line.replace(svaasOPDHeroConstant, "\"opd_Hero\"");
             pw.println(line);
         }
-        pw.println("]);");
+        pw.println("]");
 
         pw.flush();
     }
